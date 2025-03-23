223e0fe04ec57efc9460cf582955ac8e84ead8d7
Umberto Domenico Ciccia <108148943+umbertocicciaa@users.noreply.github.com>
2025-03-23 10:44:31 +0100

    add providers.tf

--- aws-provisioning/main.tf
+++ aws-provisioning/main.tf
@@ -1,17 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  profile = "default"
-  region  = "us-west-2"
-}
-
 resource "aws_instance" "my_instance" {
   ami           = "ami-0b6d6dacf350ebc82"
   instance_type = var.instance_type
