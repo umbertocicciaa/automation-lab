d4100d31c2c4cad4d5fd86eafe7b11948a2f855e
umbertocicciaa <umbertociccia@icloud.com>
2025-03-24 10:29:49 +0100

    init gcp provisioning of vm and bucket and vpc

---
+++ gcp-provisioning/vm.tf
@@ -0,0 +1,36 @@
+resource "google_compute_network" "compute_vpc" {
+  name                    = var.vpc_name
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "compute_subnet" {
+  name                     = var.subnet_name
+  ip_cidr_range            = var.ip_cidr
+  region                   = var.project_region
+  network                  = google_compute_network.compute_vpc.id
+  private_ip_google_access = true
+}
+
+resource "google_compute_instance" "instance" {
+  name                      = var.vm_name
+  zone                      = "${var.project_region}-a"
+  machine_type              = var.machine_type
+  allow_stopping_for_update = true
+
+  network_interface {
+    network    = "compute_vpc"
+    subnetwork = "compute_subnet"
+  }
+
+  boot_disk {
+    initialize_params {
+      image = var.machine_image
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+
+}
