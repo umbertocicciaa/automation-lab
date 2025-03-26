a4f20c37e6256a04cb9a92732fb6512b1ee7e118
umbertocicciaa <umbertociccia@icloud.com>
2025-03-26 09:28:59 +0100

    for_each in gcp module

--- gcp-module/main.tf
+++ gcp-module/main.tf
@@ -3,16 +3,19 @@ provider "google" {
   region  = var.project_region
 }
 
-module "vm-buckt" {
+module "vm-bucket" {
+
+  for_each = { for k, v in var.parameters : k => v }
+
   source = "./modules/vm-bucket-module"
 
-  project_id = var.project_id
-  project_region = var.project_region
-  bucket_location = var.bucket_location
-  vpc_name = var.vpc_name
-  subnet_name = var.subnet_name
-  ip_cidr = var.ip_cidr
-  vm_name = var.vm_name
-  machine_type = var.machine_type
-  machine_image = var.machine_image
+  project_id      = var.project_id
+  project_region  = var.project_region
+  bucket_location = each.value.bucket_location
+  vpc_name        = each.value.vpc_name
+  subnet_name     = each.value.subnet_name
+  ip_cidr         = each.value.ip_cidr
+  vm_name         = each.value.vm_name
+  machine_type    = each.value.machine_type
+  machine_image   = each.value.machine_image
 }
